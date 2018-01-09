952e2c4419de91e68f89d209be7dee80c6363c6c
lenziten <lenziten@hotmail.com>
2018-01-09 14:59:35 +0800

    commit

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -6,16 +6,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 //@Configuration
 //@RestController
 @MapperScan("com.dao")
-@EnableEurekaServer
 @EnableAutoConfiguration//(exclude={MapperScannerConfigurer.class})
 //@SpringBootApplication
+@EnableConfigServer
 @ComponentScan
 public class Application implements EmbeddedServletContainerCustomizer{
 
